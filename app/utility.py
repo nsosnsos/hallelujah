c97672a4dad245124a125e5ba82607f2af9e4171
nsos <nsosnsos@gmail.com>
2019-09-30 17:24:53 +0800

    Create utility.py

---
+++ app/utility.py
@@ -0,0 +1,36 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import bleach
+from markdown import markdown
+
+
+def split_key_words(keywords):
+    return keywords.replace(u',', ' ') \
+        .replace(u';', ' ') \
+        .replace(u'+', ' ') \
+        .replace(u'；', ' ') \
+        .replace(u'，', ' ') \
+        .replace(u'　', ' ') \
+        .split(' ')
+
+
+def string_to_url(string):
+    return string.replace(u' ', '+') \
+        .replace(u',', '+') \
+        .replace(u'　', '+') \
+        .replace(u'，', '+')
+
+
+def markdown2html(text):
+    allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                    'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                    'h1', 'h2', 'h3', 'p', 'img']
+    extensions = ['fenced_code', 'codehilite(css_class=highlight, linenums=true)', 'admonition', 'tables', 'extra']
+    return bleach.linkify(bleach.clean(
+        markdown(text, extensions, output_format='html5'), tags=allowed_tags, strip=True))
+
+
+# noinspection PyUnusedLocal
+def unused_param(*args, **kwargs):
+    pass
