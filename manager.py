bc30b1d59f6e98397ea2f5e74ccc56e1113b2a5b
nsos <nsosnsos@gmail.com>
2019-09-30 17:18:07 +0800

    Create manager.py

---
+++ manager.py
@@ -0,0 +1,43 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import unittest
+from flask_script import Manager, Shell, Server
+from flask_migrate import Migrate, MigrateCommand
+
+from app import create_app, db
+from app.models import Role, User, Tag, Diary, Article, Gallery, Comment
+
+
+app = create_app()
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, Role=Role, User=User,
+                Tag=Tag, Diary=Diary, Gallery=Gallery, Article=Article, Comment=Comment)
+
+
+manager.add_command('db', MigrateCommand)
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('runserver', Server(host=app.config['SYS_HOST'], port=app.config['SYS_PORT'],
+                                        use_debugger=app.config['DEBUG']))
+
+
+@manager.command
+def test():
+    test_set = unittest.TestLoader().discover('test')
+    unittest.TextTestRunner(verbosity=2).run(test_set)
+
+
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    Role.insert_roles()
+    User.insert_admin()
+
+
+if __name__ == '__main__':
+    manager.run()
