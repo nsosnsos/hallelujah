1ddc7a79e956b8f38228d8dfcf14220cf0c3f5f4
nsos <nsosnsos@gmail.com>
2019-09-30 17:20:18 +0800

    Create test_model.py

---
+++ test/test_model.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import unittest
+
+from app import create_app, db
+from app.models import User, AnonymousUser, Permission
+
+
+class TestModel(unittest.TestCase):
+    def setup(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_user(self):
+        u = User(name='test', email='test@gmail.com', password='test')
+        self.assertTrue(u.can(Permission.DIARY))
+        self.assertFalse(u.can(Permission.GALLERY))
+
+    def test_anonymous(self):
+        u = AnonymousUser()
+        self.assertTrue(u.can(Permission.ARTICLE))
