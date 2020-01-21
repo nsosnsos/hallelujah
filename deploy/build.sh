dc2fc6f782a28706777b51f33d312f768de2985c
nsos <nsosnsos@gmail.com>
2020-01-21 16:42:19 +0800

    Update build.sh

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 set -x
@@ -8,20 +8,19 @@ svc_name=hallelujah
 deploy_dir=/root/${svc_name}
 
 root_dir=$(dirname $(dirname "$(readlink -f $0)"))
-cert_dir=/etc/nginx/cert
+cert_dir=${deploy_dir}/cert
 cert_gen_sh=${root_dir}/deploy/cert_gen.sh
 nginx_conf=${root_dir}/deploy/nginx.conf
 req_pkg_txt=${root_dir}/requirements.txt
 
-
 function usage(){
 	echo "Usage: build.sh [deploy|clean]"
-	exit 1
+	exit -1
 }
 
 function cert_fail(){
 	echo "Certificates build failed!"
-	exit 1
+	exit -1
 }
 
 if [ "$#" -gt 1 ]; then
@@ -40,22 +39,34 @@ fi
 
 
 if [ $op == "deploy" ]; then
+	pip3 install -r ${req_pkg_txt}
+
 	mkdir -p ${cert_dir}
 	/bin/sh ${cert_gen_sh} ${cert_dir}
 	if [ "$?" -ne 0 ]; then
 		cert_fail
 	fi
-	mkdir -p ${deploy_dir}
+
+	cert_file=${cert_dir}/cert.pem
+	key_file=${cert_dir}/key.pem
 	cp -rf ${nginx_conf} /etc/nginx/sites-available/${svc_name}
+	sed -i -e "s/ssl_cert_path/${cert_file//\//\\/}/g" /etc/nginx/sites-available/${svc_name}
+	sed -i -e "s/ssl_key_path/${key_file//\//\\/}/g" /etc/nginx/sites-available/${svc_name}
 	ln -sf /etc/nginx/sites-available/${svc_name} /etc/nginx/sites-enabled/${svc_name}
-	cp -rf ${root_dir}/* ${deploy_dir}/
-	pip3 install -r ${req_pkg_txt}
-	systemctl daemon-reload
-	service nginx restart
+	mkdir -p ${deploy_dir}
+	cp -rf ${root_dir}/app ${deploy_dir}/
+	cp -rf ${root_dir}/config.py ${deploy_dir}/
+	cp -rf ${root_dir}/manager.py ${deploy_dir}/
+	cp -rf ${root_dir}/site.conf ${deploy_dir}/
+	sed -i -e "s/ssl_cert_path/${cert_file//\//\\/}/g" ${deploy_dir}/site.conf
+	sed -i -e "s/ssl_key_path/${key_file//\//\\/}/g" ${deploy_dir}/site.conf
+    systemctl daemon-reload
+    service nginx restart
+    cd ${deploy_dir} && python3 ${deploy_dir}/manager.py runserver
 else
 	rm -rf ${cert_dir}
 	rm -rf /etc/nginx/sites-available/${svc_name} /etc/nginx/sites-enabled/${svc_name}
 	rm -rf ${deploy_dir}
-	systemctl daemon-reload
-	service nginx restart
+    systemctl daemon-reload
+    service nginx stop
 fi
