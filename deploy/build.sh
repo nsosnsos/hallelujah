cdde4e493a79abf9fd6b8fdf4f45228d0b452c49
nsos <nsosnsos@gmail.com>
2020-01-21 16:44:01 +0800

    Update build.sh

--- deploy/build.sh
+++ deploy/build.sh
@@ -14,59 +14,59 @@ nginx_conf=${root_dir}/deploy/nginx.conf
 req_pkg_txt=${root_dir}/requirements.txt
 
 function usage(){
-	echo "Usage: build.sh [deploy|clean]"
-	exit -1
+    echo "Usage: build.sh [deploy|clean]"
+    exit -1
 }
 
 function cert_fail(){
-	echo "Certificates build failed!"
-	exit -1
+    echo "Certificates build failed!"
+    exit -1
 }
 
 if [ "$#" -gt 1 ]; then
-	usage
+    usage
 elif [ "$#" -eq 1 ]; then
-	if [ "$1" == "deploy" ]; then
-		op="deploy"
-	elif [ "$1" == "clean" ]; then
-		op="clean"
-	else
-		usage
-	fi
+    if [ "$1" == "deploy" ]; then
+        op="deploy"
+    elif [ "$1" == "clean" ]; then
+        op="clean"
+    else
+        usage
+    fi
 else
-	op="deploy"
+    op="deploy"
 fi
 
 
 if [ $op == "deploy" ]; then
-	pip3 install -r ${req_pkg_txt}
+    pip3 install -r ${req_pkg_txt}
 
-	mkdir -p ${cert_dir}
-	/bin/sh ${cert_gen_sh} ${cert_dir}
-	if [ "$?" -ne 0 ]; then
-		cert_fail
-	fi
+    mkdir -p ${cert_dir}
+    /bin/sh ${cert_gen_sh} ${cert_dir}
+    if [ "$?" -ne 0 ]; then
+        cert_fail
+    fi
 
-	cert_file=${cert_dir}/cert.pem
-	key_file=${cert_dir}/key.pem
-	cp -rf ${nginx_conf} /etc/nginx/sites-available/${svc_name}
-	sed -i -e "s/ssl_cert_path/${cert_file//\//\\/}/g" /etc/nginx/sites-available/${svc_name}
-	sed -i -e "s/ssl_key_path/${key_file//\//\\/}/g" /etc/nginx/sites-available/${svc_name}
-	ln -sf /etc/nginx/sites-available/${svc_name} /etc/nginx/sites-enabled/${svc_name}
-	mkdir -p ${deploy_dir}
-	cp -rf ${root_dir}/app ${deploy_dir}/
-	cp -rf ${root_dir}/config.py ${deploy_dir}/
-	cp -rf ${root_dir}/manager.py ${deploy_dir}/
-	cp -rf ${root_dir}/site.conf ${deploy_dir}/
-	sed -i -e "s/ssl_cert_path/${cert_file//\//\\/}/g" ${deploy_dir}/site.conf
-	sed -i -e "s/ssl_key_path/${key_file//\//\\/}/g" ${deploy_dir}/site.conf
+    cert_file=${cert_dir}/cert.pem
+    key_file=${cert_dir}/key.pem
+    cp -rf ${nginx_conf} /etc/nginx/sites-available/${svc_name}
+    sed -i -e "s/ssl_cert_path/${cert_file//\//\\/}/g" /etc/nginx/sites-available/${svc_name}
+    sed -i -e "s/ssl_key_path/${key_file//\//\\/}/g" /etc/nginx/sites-available/${svc_name}
+    ln -sf /etc/nginx/sites-available/${svc_name} /etc/nginx/sites-enabled/${svc_name}
+    mkdir -p ${deploy_dir}
+    cp -rf ${root_dir}/app ${deploy_dir}/
+    cp -rf ${root_dir}/config.py ${deploy_dir}/
+    cp -rf ${root_dir}/manager.py ${deploy_dir}/
+    cp -rf ${root_dir}/site.conf ${deploy_dir}/
+    sed -i -e "s/ssl_cert_path/${cert_file//\//\\/}/g" ${deploy_dir}/site.conf
+    sed -i -e "s/ssl_key_path/${key_file//\//\\/}/g" ${deploy_dir}/site.conf
     systemctl daemon-reload
     service nginx restart
     cd ${deploy_dir} && python3 ${deploy_dir}/manager.py runserver
 else
-	rm -rf ${cert_dir}
-	rm -rf /etc/nginx/sites-available/${svc_name} /etc/nginx/sites-enabled/${svc_name}
-	rm -rf ${deploy_dir}
+    rm -rf ${cert_dir}
+    rm -rf /etc/nginx/sites-available/${svc_name} /etc/nginx/sites-enabled/${svc_name}
+    rm -rf ${deploy_dir}
     systemctl daemon-reload
     service nginx stop
 fi
