f2d2366f532ec1d4e245566238ddcd01ebe70b92
nsosnsos <nsosnsos@gmail.com>
2020-02-04 09:38:08 +0800

    FEAT: USE FONT AWESOME AS LOCAL RESOURCE
    add font awesome 5.12.0 to project as local resource.

--- app/main/errors.py
+++ app/main/errors.py
@@ -1,7 +1,7 @@
 # !/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request, jsonify
 
 from . import main
 from ..utility import unused_param
@@ -10,16 +10,31 @@ from ..utility import unused_param
 @main.app_errorhandler(403)
 def forbidden_error(e):
     unused_param(e)
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'forbidden'})
+        response.status_code = 403
+        return response
     return render_template('403.html'), 403
 
 
 @main.app_errorhandler(404)
 def page_not_found(e):
     unused_param(e)
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'not found'})
+        response.status_code = 404
+        return response
     return render_template('404.html'), 404
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
     unused_param(e)
+    if request.accept_mimetypes.accept_json and \
+            not request.accept_mimetypes.accept_html:
+        response = jsonify({'error': 'internal server error'})
+        response.status_code = 500
+        return response
     return render_template('500.html'), 500
