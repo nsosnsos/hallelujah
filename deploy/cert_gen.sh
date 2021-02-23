0f26f9bc7241f727c28876afe4f3e420da238b7e
nsos <nsosnsos@gmail.com>
2021-02-23 15:09:04 +0800

    FEAT: ADD OPENSSL REQ SUBJ
    add open-ssl req subject parameters.

--- deploy/cert_gen.sh
+++ deploy/cert_gen.sh
@@ -3,20 +3,12 @@
 set -e
 set -x
 
-if [ "$#" -eq 1 ]; then
-	cert_dir=$1
-	echo $cert_dir
+if [ "${#}" -eq 1 ]; then
+	cert_dir=${1}
+	echo "${cert_dir}"
 	apt-get install openssl
-	openssl req -x509 -newkey rsa:4096 -nodes -out ${cert_dir}/cert.pem -keyout ${cert_dir}/key.pem -days 1000 << 'EOF'
-CN
-Shaanxi
-Xi'an
-.
-.
-Stan Lee
-test@gmail.com
-EOF
+	openssl req -x509 -newkey rsa:4096 -nodes -out "${cert_dir}"/cert.pem -keyout "${cert_dir}"/key.pem -days 9999 -subj "/C=CN/ST=Shaanxi/L=Xi'an/O=Global Security/OU=IT Department/CN=test@gmail.com"
 else
 	echo "Error: no cert path specified!"
-	exit -1
+	exit 1
 fi
