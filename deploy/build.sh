f9f03b63cbe4ef47da1f04dd6ae70b7bb70f814a
nsos <nsosnsos@gmail.com>
2021-02-23 11:13:02 +0800

    REFACTOR: ENHANCE SHELL
    shell script enhancement.

--- deploy/build.sh
+++ deploy/build.sh
@@ -7,7 +7,7 @@ set -x
 svc_name=hallelujah
 deploy_dir=/home/${svc_name}
 
-root_dir=$(dirname $(dirname "$(readlink -f $0)"))
+root_dir=$(dirname "$(dirname "$(readlink -f "${0}")")")
 cert_dir=${deploy_dir}/cert
 cert_gen_sh=${root_dir}/deploy/cert_gen.sh
 nginx_conf=${root_dir}/deploy/nginx.conf
@@ -15,12 +15,12 @@ req_pkg_txt=${root_dir}/requirements.txt
 
 function usage(){
     echo "Usage: build.sh [deploy|clean]"
-    exit -1
+    exit 1
 }
 
 function cert_fail(){
     echo "Certificates build failed!"
-    exit -1
+    exit 1
 }
 
 if [ "$#" -gt 1 ]; then
@@ -39,27 +39,26 @@ fi
 
 
 if [ $op == "deploy" ]; then
-    pip3 install -r ${req_pkg_txt}
+    pip3 install -r "${req_pkg_txt}"
 
     mkdir -p ${cert_dir}
     cert_file=${cert_dir}/cert.pem
     key_file=${cert_dir}/key.pem
-	if [ ! -f ${cert_file} ] || [ ! -f ${key_file} ]; then
-	    /bin/sh ${cert_gen_sh} ${cert_dir}
-		if [ "$?" -ne 0 ]; then
-			cert_fail
-		fi
-	fi
+	  if [ ! -f ${cert_file} ] || [ ! -f ${key_file} ]; then
+	      if /bin/sh "${cert_gen_sh}" ${cert_dir}; then
+		        cert_fail
+		    fi
+	  fi
 
-    cp -rf ${nginx_conf} /etc/nginx/sites-available/${svc_name}
+    cp -rf "${nginx_conf}" /etc/nginx/sites-available/${svc_name}
     sed -i -e "s/ssl_cert_path/${cert_file//\//\\/}/g" /etc/nginx/sites-available/${svc_name}
     sed -i -e "s/ssl_key_path/${key_file//\//\\/}/g" /etc/nginx/sites-available/${svc_name}
     ln -sf /etc/nginx/sites-available/${svc_name} /etc/nginx/sites-enabled/${svc_name}
     mkdir -p ${deploy_dir}
-    cp -rf ${root_dir}/app ${deploy_dir}/
-    cp -rf ${root_dir}/config.py ${deploy_dir}/
-    cp -rf ${root_dir}/manager.py ${deploy_dir}/
-    cp -rf ${root_dir}/site.conf ${deploy_dir}/
+    cp -rf "${root_dir}"/app ${deploy_dir}/
+    cp -rf "${root_dir}"/config.py ${deploy_dir}/
+    cp -rf "${root_dir}"/manager.py ${deploy_dir}/
+    cp -rf "${root_dir}"/site.conf ${deploy_dir}/
     sed -i -e "s/ssl_cert_path/${cert_file//\//\\/}/g" ${deploy_dir}/site.conf
     sed -i -e "s/ssl_key_path/${key_file//\//\\/}/g" ${deploy_dir}/site.conf
     systemctl daemon-reload
