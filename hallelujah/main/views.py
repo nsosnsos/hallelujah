fae26d02a976973512d158f75c4935a33dd76176
nsosnsos <nsosnsos@gmail.com>
2025-09-04 17:45:37 +0800

    FIX: ENABLED ADD RESOURCE WITH SHORT NAME
    enabled resource directory short name by setting min length.
    closes #174

--- hallelujah/main/views.py
+++ hallelujah/main/views.py
@@ -131,7 +131,7 @@ def manage_medias(current_path):
         os.makedirs(target_path, mode=0o775, exist_ok=True)
         dirs.append(target_dir)
         dirs.sort()
-        flash('Directory ' + current_path + ' is added successfully!')
+        flash('Directory ' + target_path + ' is added successfully!')
     return render_template('main/medias.html', current_path=current_path, form=form, dirs=dirs, files=files)
 
 @bp_main.route('/upload/<path:current_path>', methods=['POST'])
